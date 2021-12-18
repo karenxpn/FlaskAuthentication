d8dc25341f1049e47b5f42abf7d15f1ae6c550ce
KALIMI <karenmirakyan@gmail.com>
2021-12-18 06:29:35 +0400

    all authentication added pass authentication status to templates left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, jsonify
+from flask_login import login_required, LoginManager, logout_user, login_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 
@@ -10,9 +9,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models.user_model import User
+from services.database_service import UserService
 
 db.init_app(app)
+service = UserService()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return service.get_user_by_id(user_id)
 
 
 @app.route('/')
@@ -20,29 +28,49 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        user = service.add_user(request.form)
+        if user:
+            login_user(user)
+            return redirect(url_for('secrets'))
+        else:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        user = service.login_user(password=request.form.get('password'),
+                                  email=request.form.get('email'))
+        if user:
+            login_user(user)
+            return redirect(url_for('secrets'))
+        else:
+            return jsonify(error={"Not found": "Please check your email and password"})
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
     return render_template("secrets.html")
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
 
 
 @app.route('/download')
+@login_required
 def download():
-    pass
+    return send_from_directory(app.static_folder, 'files/cheat_sheet.pdf')
 
 
 if __name__ == "__main__":
--- services/database_service.py
+++ services/database_service.py
@@ -1,5 +1,37 @@
 from models.user_model import User, db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class UserService:
-    pass
+    def add_user(self, form):
+
+        user = User.query.filter_by(email=form.get('email')).first()
+
+        if user:
+            return None
+        else:
+            hash_password = generate_password_hash(form.get('password'),
+                                                   method='pbkdf2:sha256',
+                                                   salt_length=8)
+            new_user = User(
+                name=form.get('name'),
+                email=form.get('email'),
+                password=hash_password
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user
+
+    def login_user(self, password, email):
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                return user
+
+        return None
+
+    def get_user_by_id(self, user_id):
+        return User.query.get(user_id)
+
